68b7e74542bb72151260c5bb2b3b87f0d38bef67
Daniel Garcia <daniel.garcia@mail.utoronto.ca>
2019-01-15 18:47:07 -0500

    Improve environment setup script
    Script now activates the virtual environment before updating pip
    and installing project requirements

--- env_setup.bat
+++ env_setup.bat
@@ -1,3 +1,10 @@
 @echo off
+echo Creating virtual environment
 python -m venv pyenv
+echo Activating virtual environment
+call pyenv\Scripts\Activate.bat
+echo Updating pip
+python -m pip install --upgrade pip
+echo Installing dependencies
 pip install -r requirements.txt
+echo Environment is set up and ready to go
